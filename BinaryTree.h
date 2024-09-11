3166d2080240a53ea0ebd492418fbdbc4ebc6999
CWOfficial <124388457+cwunch02@users.noreply.github.com>
2024-09-10 20:34:05 -0400

    Upload project files

---
+++ BinaryTree.h
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class Node{
+
+  //Each node should have
+
+  //- pointer to the parent node
+
+  //- pointer to left child
+
+  //- pointer to right child
+
+  //- int for storing the key value
+
+public:
+
+  //constructor
+  //remember to always inizialize pointers to NULL
+
+  //getter and setter for nodes and value
+
+};
+
+class BinaryTree{
+
+protected:
+  //A tree should store only its root.
+  Node* root;
+
+public:
+  //functions required
+
+  //- constructor
+
+  //- getter setter root
+
+  //- a function for printing the inorder visit of the tree
+
+  //- a function for inserting a new value in the tree
+
+  //- a function for searching a value in the tree.
+
+};
